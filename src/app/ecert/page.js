f534dde65903ff1e32a3104559cf46db336a6c0a
misxabien <misxabien23@gmail.com>
2026-05-10 17:47:29 +0800

    Add admin analytics Next.js app (routes, views, shared styles)

---
+++ src/app/ecert/page.js
@@ -0,0 +1,9 @@
+import { EcertView } from "@/components/views/EcertView";
+
+export const metadata = {
+  title: "E-Certificate",
+};
+
+export default function EcertPage() {
+  return <EcertView />;
+}
