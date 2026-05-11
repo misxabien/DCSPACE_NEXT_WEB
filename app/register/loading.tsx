de84c66543705eff8c87d0852f9de2880c17ff6f
Khrystelle Esplana <khrstleesplna@gmail.com>
2026-05-12 01:57:48 +0800

    Provided loading page indicator but modified the design for registering accounts.

---
+++ app/register/loading.tsx
@@ -0,0 +1,9 @@
+import { BrandedLoading } from "@/components/BrandedLoading";
+
+export default function Loading() {
+  return (
+    <div className="login-scope">
+      <BrandedLoading label="Loading registration..." />
+    </div>
+  );
+}
