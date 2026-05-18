1d0081dcd76bf606932bec29df9f316591afc5af
Khrystelle Esplana <khrstleesplna@gmail.com>
2026-05-18 18:45:11 +0800

    Added and organized new svg icons for modernized UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,15 +42,15 @@ export const metadata: Metadata = {
   description: 'TAP. ATTEND. GET CERTIFIED.',
   icons: {
     icon: {
-      url: "/dcspace-logo-circle.png",
-      type: "image/png",
-      sizes: "32x32",
+      url: '/dcspace-logo-circle.png',
+      type: 'image/png',
+      sizes: '32x32',
     },
-    shortcut: "/dcspace-logo-circle.png",
+    shortcut: '/dcspace-logo-circle.png',
     apple: {
-      url: "/dcspace-logo-circle.png",
-      type: "image/png",
-      sizes: "180x180",
+      url: '/dcspace-logo-circle.png',
+      type: 'image/png',
+      sizes: '180x180',
     },
   },
 };
@@ -62,6 +62,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cal+Sans&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Google+Sans+Flex:opsz,wght@6..144,1..1000&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={`${dmSans.variable} ${lato.variable} ${montserrat.variable} ${poppins.variable} ${lato.className}`}>
         {children}
       </body>
