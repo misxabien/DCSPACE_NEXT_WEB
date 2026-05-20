a10d73df9c8e74f3cc358de12431c3cd27b62456
Khrystelle Esplana <khrstleesplna@gmail.com>
2026-05-21 06:06:42 +0800

    Redesigned the loading page.

---
+++ app/(main)/attendance/details/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingScreen } from '@/components/LoadingScreen';
+
+export default function AttendanceDetailsLoading() {
+  return <LoadingScreen context="attendance-details" />;
+}
---
+++ app/(main)/certificates/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingScreen } from '@/components/LoadingScreen';
+
+export default function CertificatesLoading() {
+  return <LoadingScreen context="certificates" />;
+}
